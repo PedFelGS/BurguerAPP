1aec58a8c8b991553a73779f08d9453c19f7fda6
PedFelGS <pedfelgs@gmail.com>
2025-01-16 03:48:39 -0300

    Renomeando rota e adicionando rota de detalhes ao produto

--- catalog_BurguerApp/urls.py
+++ catalog_BurguerApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('produtos/', views.product_list, name='product_list'),
-    path('produtos/create', views.product_create, name='product_create')
+    path('produtos/<int:id>/', views.product_detail, name='product_detail'),
+    path('produtos/criar/', views.product_create, name='product_create'),
 ]
