35536825804bbe4605d385024bf5f7b38fd42f5e
PedFelGS <pedfelgs@gmail.com>
2025-01-16 02:22:21 -0300

    feat(settings): Configurando diretórios de templates e arquivos estáticos atualizando o arquivo 'settings.py' para refletir essa configuração
    - Adicionado o diretório 'templates' na configuração 'TEMPLATES' e atualizado o campo 'DIRS' para referenciar o diretório usando 'os.path.join'.
    - Incluído suporte a arquivos estáticos no diretório 'templates/static' definindo em 'STATICFILES_DIRS'.
    - Definido 'STATIC_ROOT' para especificar o local dos arquivos estáticos coletados, garantindo suporte ao gerenciamento durante a implementação.

--- BurguerApp/settings.py
+++ BurguerApp/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'BurguerApp.urls'
 
+import os;
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates'),   ], # Diretório onde seus templates estão armazenados
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "templates" / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
