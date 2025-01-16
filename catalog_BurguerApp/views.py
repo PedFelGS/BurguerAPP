e37890912dc2a4fefaba5c8090928f1359796a5c
PedFelGS <pedfelgs@gmail.com>
2025-01-16 02:29:28 -0300

    feat(views): Adicionar visualização de produtos
    Implementação da view para exibir uma lista de produtos no template HTML. A consulta aos produtos é realizada com a obtenção de todos os itens no banco de dados. A renderização é feita utilizando o template 'list.html'.

--- catalog_BurguerApp/views.py
+++ catalog_BurguerApp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect;
+from django.http import HttpResponse
+from .models import Products, Categories, User, Orders, OrderItem 
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def product(req):
+  if req.method == "GET":
+    products = Products.objects.all()
+    print(products)
+    return render(req, './products/list.html', {"products": products} )
+  
+    '''
+    # Test: serializando os produtos em um formato que possa ser convertido para JSON
+    product_list = list(products.values('id', 'name', 'price', 'category__name', 'path', 'offer'))
+    return JsonResponse({"products": product_list})
+    '''
+  
+  
