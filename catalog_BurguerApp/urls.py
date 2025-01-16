36a3ea4e486c51319dbe57c7eb789be447be5d30
PedFelGS <pedfelgs@gmail.com>
2025-01-16 05:30:05 -0300

    Adicionando endpoint responsável pelo exclusão de produtos

--- catalog_BurguerApp/urls.py
+++ catalog_BurguerApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('produtos/<int:id>/', views.product_detail, name='product_detail'),
     path('produtos/criar/', views.product_create, name='product_create'),
     path('produtos/<int:id>/atualizar/', views.product_update, name='product_update'),
+    path('produtos/<int:id>/excluir/', views.product_delete, name='product_delete'),
 ]
