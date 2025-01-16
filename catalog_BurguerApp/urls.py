1a6268b89ecec14686f6678a3bd9c3a551ad9497
PedFelGS <pedfelgs@gmail.com>
2025-01-16 03:14:35 -0300

    Separando responsabilidades, '/produtos' agora só irá listar os produtos

--- catalog_BurguerApp/urls.py
+++ catalog_BurguerApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path;
 from . import views
 
 urlpatterns = [
-    path('produtos/', views.product, name='product'),
+    path('produtos/', views.product_list, name='product_list'),
 ]
