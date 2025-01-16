fef54712a730843b028479d715f1b054340ba136
PedFelGS <pedfelgs@gmail.com>
2025-01-16 04:08:15 -0300

    Adicionando endpoint para atualização de produto

--- catalog_BurguerApp/urls.py
+++ catalog_BurguerApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('produtos/', views.product_list, name='product_list'),
     path('produtos/<int:id>/', views.product_detail, name='product_detail'),
     path('produtos/criar/', views.product_create, name='product_create'),
+    path('produtos/<int:id>/atualizar/', views.product_update, name='product_update'),
 ]
