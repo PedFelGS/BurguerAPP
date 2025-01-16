d24cbfc320d1389140968f69f33c778b2d396e77
PedFelGS <pedfelgs@gmail.com>
2025-01-16 03:23:07 -0300

    feat(product): Adicionar funcionalidade de criação de produto
    Implementação da funcionalidade para criação de um novo produto através de um formulário. Verificação se o método da requisição é POST, com validação e salvamento do produto, redirecionando para a lista de produtos após sucesso.
    
    - Integração do formulário ProductForm para criação e validação de dados.
    - Redirecionamento para a lista de produtos após sucesso no envio do formulário.

--- catalog_BurguerApp/views.py
+++ catalog_BurguerApp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect;
 from django.http import HttpResponse
-from .models import Products, Categories, User, Orders, OrderItem 
+from .models import Products, Categories, User, Orders, OrderItem
+from .forms import ProductForm
 
 # Create your views here.
 
 from django.http import HttpResponse
 
-def product(req):
+def product_list(req):
   if req.method == "GET":
     products = Products.objects.all()
     print(products)
@@ -16,6 +17,15 @@ def product(req):
     # Test: serializando os produtos em um formato que possa ser convertido para JSON
     product_list = list(products.values('id', 'name', 'price', 'category__name', 'path', 'offer'))
     return JsonResponse({"products": product_list})
-    '''
-  
-  
+    ''' 
+
+def product_create(req):
+  if req.method == "POST":
+    form = ProductForm(req.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('product_list')
+  else:
+    form = ProductForm()
+  return render(req, 'products/form.html', {'form': form})
+      
