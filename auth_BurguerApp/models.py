42f6667a4160c8f45ff9a5aa57b88f1d077dcd4b
PedFelGS <pedfelgs@gmail.com>
2025-01-18 04:13:57 -0300

    chore(database): Apagando comentários para melhorar a legibilidade

--- auth_BurguerApp/models.py
+++ auth_BurguerApp/models.py
@@ -17,16 +17,3 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
-
-'''
-
-Comandos SQLITE para a tabela User:
-
-1. Inserir um usuário:
-INSERT INTO auth_burguerapp_user (id, name, email, password_hash, admin, created_at, updated_at) 
-VALUES (hex(randomblob(16)), 'João Silva', 'joao.silva@example.com', 'hash_senha1', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-2. Consulta de todos os usuários:
-SELECT * FROM auth_burguerapp_user
-
-'''
