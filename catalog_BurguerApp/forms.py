4324a12b584e49db2c57ed6fc4b284867b5f34ab
PedFelGS <pedfelgs@gmail.com>
2025-01-17 23:38:58 -0300

    feat(forms): Adicionar campos offer e path ao ProductForm e a implementação de CategoryForm
    Adiciona os campos 'offer' (BooleanField) e 'path' (URLField) ao ProductForm para representar uma oferta e armazenar a URL de uma imagem externa. Além disso, implementa completamente o CategoryForm, com os campos 'name' e 'path', onde o campo 'path' armazena a URL de uma imagem para a categoria.
    
    - No ProductForm, o campo 'offer' é um checkbox para indicar ofertas especiais.
    - O campo 'path' no ProductForm permite associar uma URL de imagem ao produto.
    - O CategoryForm foi completamente implementado com os campos 'name' (para o nome da categoria) e 'path' (para a URL da imagem da categoria).

--- catalog_BurguerApp/forms.py
+++ catalog_BurguerApp/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from .models import Products
+from .models import Products, Categories;
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Products
         fields = ['name', 'price', 'category', 'path', 'offer']
+        offer = forms.BooleanField(
+            required=False,
+            widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        )
+        path = forms.URLField(
+            required=False,
+            widget=forms.URLInput(attrs={'class': 'form-control'}),
+            label="URL da Imagem",
+            help_text="Insira a URL de uma imagem externa para o produto"
+        )
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Categories
+        fields = ['name', 'path'] 
