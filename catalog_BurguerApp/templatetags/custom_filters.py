5263d3d54f59ca22d5d617052f102c64347e7ced
PedFelGS <pedfelgs@gmail.com>
2025-01-18 04:36:53 -0300

    chore(filter): Apagando comentários para melhorar a legibilidade

--- catalog_BurguerApp/templatetags/custom_filters.py
+++ catalog_BurguerApp/templatetags/custom_filters.py
@@ -5,7 +5,5 @@ register = template.Library()
 
 @register.filter
 def add_class(field, css_class):
-    """
-    Adiciona uma classe CSS ao campo de formulário.
-    """
+
     return field.as_widget(attrs={"class": css_class})
