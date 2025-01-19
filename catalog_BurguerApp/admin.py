4f7f921621f183fb8eed2691a9daded12892f3bd
PedFelGS <pedfelgs@gmail.com>
2025-01-19 08:40:04 -0300

    feat(admin): Adicionar painel de administração para Categorias e Produtos
    - Adição de painel administrativo para as classes Categorias e Produtos.
    - Configuração de visualização, filtros e ordenação para os modelos.
    - Melhora na experiência do usuário com filtros dinâmicos para categorias e ofertas.

--- catalog_BurguerApp/admin.py
+++ catalog_BurguerApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categories, Products, Orders, OrderItem
 
-# Register your models here.
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('figure','name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('figure','name', 'price', 'category', 'offer', 'created_at', 'updated_at')
+    list_editable = ('price', 'offer')
+    search_fields = ('name',)
+    list_filter = ('category', 'offer')
+    ordering = ('name',)
