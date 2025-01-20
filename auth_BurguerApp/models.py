4c0b21cab1c1a46c3fb8a7156014620158f2031f
PedFelGS <pedfelgs@gmail.com>
2025-01-20 10:33:28 -0300

    feat(auth): Adicionar campo 'role' e métodos para hash de senha
    - Adicionado campo 'role' no modelo de usuário, com opções de 'cliente' e 'gerenciador'.
    - Implementados métodos para definir e verificar senhas com hash seguro.

--- auth_BurguerApp/models.py
+++ auth_BurguerApp/models.py
@@ -1,8 +1,13 @@
 import uuid
 from django.db import models
-from django.db.models.fields import CharField, EmailField, BooleanField, DateTimeField, UUIDField, AutoField;
+from django.contrib.auth.hashers import make_password, check_password
+from django.db.models.fields import CharField, EmailField, BooleanField, DateTimeField, UUIDField;
 
 class User(models.Model):
+    ROLE_CHOICES = [
+        ('cliente', 'Cliente'),
+        ('gerenciador', 'Gerenciador'),
+    ]
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
@@ -12,8 +17,16 @@ class User(models.Model):
     email = EmailField(unique=True)
     password_hash = CharField(max_length=255)
     admin = BooleanField(default=False)
+    role = CharField(max_length=20, choices=ROLE_CHOICES, default='cliente')
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
 
+    def set_password(self, raw_password):
+        self.password_hash = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password_hash)
+
     def __str__(self):
         return self.name
+    
