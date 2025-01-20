c30ffae600310e1000529e4f569637b6955b1e18
PedFelGS <pedfelgs@gmail.com>
2025-01-20 10:38:56 -0300

    feat(urls): Adicionar sistema de autenticação e ajustes nas rotas
    - Adicionada a inclusão de rotas para o módulo 'auth_BurguerApp' com a URL 'auth/'
    - Alterada a URL 'catalog_BurguerApp' para 'system/'
    - Implementada página de perfil de usuário
    - Configuração de tratamentos personalizados para erros 404 e 500

--- BurguerApp/urls.py
+++ BurguerApp/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for BurguerApp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,12 +6,17 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog_BurguerApp/', include('catalog_BurguerApp.urls')),
+    path('system/', include('catalog_BurguerApp.urls')),
+    path('auth/', include('auth_BurguerApp.urls')),
     path('', views.home, name='home'),
     path('cart/', views.view_cart, name='view_cart'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('profile/', views.user_profile, name='user_profile'),
 ]
 
+handler404 = 'BurguerApp.views.custom_page_not_found'
+handler500 = 'BurguerApp.views.custom_error'
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
