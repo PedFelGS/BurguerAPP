2ebbfe1089796cf021fd1cbe7071b6fad56b560b
PedFelGS <pedfelgs@gmail.com>
2025-01-25 10:29:09 -0300

    test(auth): Adicionar testes de URL para autenticação
    - Teste para garantir que as URLs de login, logout e registro estejam corretas
    - Verificação do correto mapeamento para 'login_view', 'logout_view', e 'register_view'

--- auth_BurguerApp/tests.py
+++ auth_BurguerApp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class AuthURLSTest(TestCase):
+    def test_burguerapp_login_url_is_correct(self):
+      login_url = reverse('auth_BurguerApp:login_view')
+      self.assertEqual(login_url, '/auth/login/')
+    
+    def test_burguerapp_logout_url_is_correct(self):
+        logout_url = reverse('auth_BurguerApp:logout_view')
+        self.assertEqual(logout_url, '/auth/logout/')
+
+    def test_burguerapp_register_url_is_correct(self):
+        register_url = reverse('auth_BurguerApp:register_view')
+        self.assertEqual(register_url, '/auth/register/')
