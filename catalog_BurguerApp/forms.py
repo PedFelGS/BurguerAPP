041d46f5da0300b9557c7bc3741b119c01af065b
PedFelGS <pedfelgs@gmail.com>
2025-01-26 10:26:39 -0300

    test(forms) Corrigindo erro de sintaxe e indentação

--- catalog_BurguerApp/forms.py
+++ catalog_BurguerApp/forms.py
@@ -1,22 +1,30 @@
 from django import forms
-from .models import Products, Categories;
+from .models import Products, Categories
 
 class ProductForm(forms.ModelForm):
+    offer = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+    path = forms.URLField(
+        required=False,
+        widget=forms.URLInput(attrs={'class': 'form-control'}),
+        label="URL da Imagem",
+        help_text="Insira a URL de uma imagem externa para o produto",
+    )
+
     class Meta:
         model = Products
         fields = ['name', 'price', 'category', 'path', 'offer']
-        offer = forms.BooleanField(
-            required=False,
-            widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
-        )
-        path = forms.URLField(
-            required=False,
-            widget=forms.URLInput(attrs={'class': 'form-control'}),
-            label="URL da Imagem",
-            help_text="Insira a URL de uma imagem externa para o produto"
-        )
 
 class CategoryForm(forms.ModelForm):
+    path = forms.URLField(
+        required=False,
+        widget=forms.URLInput(attrs={'class': 'form-control'}),
+        label="URL da Categoria",
+        help_text="Insira a URL de uma imagem externa para a categoria",
+    )
+
     class Meta:
         model = Categories
-        fields = ['name', 'path'] 
+        fields = ['name', 'path']
